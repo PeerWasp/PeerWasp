e2002e65ca1509cc1364ab1ce5976a7e4cc185be
Andreas Albrecht <albrecht@iodir.com>
2014-07-15 10:23:22 +0200

    navigator continued

--- peerbox/src/main/java/org/peerbox/controller/MainNavigator.java
+++ peerbox/src/main/java/org/peerbox/controller/MainNavigator.java
@@ -1,13 +1,17 @@
 package org.peerbox.controller;
 
+
 import java.io.IOException;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 
 public class MainNavigator {
 	
 	private static MainController mainController;
+	private static ObservableList<Node> pages = FXCollections.observableArrayList();
 	
 	public static void setMainController(MainController controller) {
 		mainController = controller;
@@ -18,8 +22,21 @@ public class MainNavigator {
 		try {
 			content = FXMLLoader.load(MainController.class.getResource(fxmlFile));
 			mainController.setContent(content);
+			pages.add(content);
 		} catch(IOException e) {
 			System.err.println("Could not load fxml file.");
+		}
+	}
+	
+	
+	public static boolean canGoBack() {
+		return pages.size() >= 2;
+	}
+	
+	public static void goBack() {
+		if(canGoBack()) {
+			int previous = pages.size()-2;
+			mainController.setContent(pages.get(previous));
 		}
 	}
 }
