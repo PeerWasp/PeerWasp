e6b3ab905d0fbaa95776779c822e411dce2b68b9
raute <claudioanliker@hotmail.com>
2014-07-15 10:56:34 +0200

    + Fixed Test / merged CreateNetworkController.java

--- peerbox/src/main/java/org/peerbox/controller/CreateNetworkController.java
+++ peerbox/src/main/java/org/peerbox/controller/CreateNetworkController.java
@@ -1,7 +1,15 @@
 package org.peerbox.controller;
 
+import java.math.BigInteger;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.UUID;
+
+import org.hive2hive.core.H2HConstants;
+import org.hive2hive.core.api.H2HNode;
+import org.hive2hive.core.api.configs.FileConfiguration;
+import org.hive2hive.core.api.configs.NetworkConfiguration;
+import org.hive2hive.core.api.interfaces.IH2HNode;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -22,9 +30,31 @@ public class CreateNetworkController implements Initializable {
 	@FXML
 	private TextField ipOutputAddress;
 	
+	private IH2HNode node;
+	private BigInteger maxFileSize = H2HConstants.DEFAULT_MAX_FILE_SIZE;
+	private int maxNumOfVersions = H2HConstants.DEFAULT_MAX_NUM_OF_VERSIONS;
+	private BigInteger maxSizeAllVersions = H2HConstants.DEFAULT_MAX_SIZE_OF_ALL_VERSIONS;
+	private int chunkSize = H2HConstants.DEFAULT_CHUNK_SIZE;
+	
 	public void goBackToSelection(ActionEvent event){
 		System.out.println("Go back.");	
 		MainNavigator.goBack();
+	}
+	
+	public void createNetwork(ActionEvent event){
+		System.out.println("Try to create network...");
+		node = H2HNode.createNode(NetworkConfiguration.create(generateNodeID()),
+				FileConfiguration.createCustom(maxFileSize, maxNumOfVersions, maxSizeAllVersions, chunkSize));
+		node.getUserManager().configureAutostart(false);
+		node.getFileManager().configureAutostart(false);
+	}
+	
+	public IH2HNode getNode(){
+		return node;
+	}
+	
+	private String generateNodeID() {
+		return UUID.randomUUID().toString();
 	}
 
 }
--- peerbox/src/test/java/org/peerbox/controller/CreateNetworkControllerTest.java
+++ peerbox/src/test/java/org/peerbox/controller/CreateNetworkControllerTest.java
@@ -1,7 +1,17 @@
 package org.peerbox.controller;
 
+import javafx.event.ActionEvent;
+
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
-public class CreateNetworkControllerTest extends TestCase {
+public class CreateNetworkControllerTest {
 
+	@Test
+	public void createNetworkTest(){
+		CreateNetworkController controller = new CreateNetworkController();
+		controller.createNetwork(new ActionEvent());
+		assert(controller.getNode() != null);
+	}
 }
