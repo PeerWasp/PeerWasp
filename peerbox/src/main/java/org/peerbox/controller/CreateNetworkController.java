2deb09188165c7dd60f65a560badb8a435c1f14e
Patrick <Patrick@89.206.66.197>
2014-07-15 10:21:28 +0200

    "Go Back" button functionality implemented in "Create Network" view

--- peerbox/src/main/java/org/peerbox/controller/CreateNetworkController.java
+++ peerbox/src/main/java/org/peerbox/controller/CreateNetworkController.java
@@ -1,5 +1,26 @@
 package org.peerbox.controller;
 
-public class CreateNetworkController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class CreateNetworkController implements Initializable {
+
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	@FXML
+	private Button btnBackToSelection;
+	
+	public void goBackToSelection(ActionEvent event){
+		System.out.println("Network created.");	
+		MainNavigator.navigate("../NetworkSelectionWindow.fxml");
+	}
 
 }
