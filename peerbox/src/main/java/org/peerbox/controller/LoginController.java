48767b05502d061e6383d43a099e953d68375ea5
Andreas Albrecht <albrecht@iodir.com>
2014-07-16 07:55:59 +0200

    small refactoring in login view

--- peerbox/src/main/java/org/peerbox/controller/LoginController.java
+++ peerbox/src/main/java/org/peerbox/controller/LoginController.java
@@ -3,10 +3,13 @@ package org.peerbox.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
@@ -22,6 +25,9 @@ public class LoginController implements Initializable {
 	private PasswordField txtPin;
 	
 	@FXML
+	private CheckBox chbAutoLogin;
+	
+	@FXML
 	private Button btnLogin;
 	
 	@FXML
@@ -29,7 +35,6 @@ public class LoginController implements Initializable {
 	
 	
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
 		
 	}
 	
