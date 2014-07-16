5a9bc08805190faa059a1d435e1f0247a5ea064d
raute <claudioanliker@hotmail.com>
2014-07-16 10:42:26 +0200

    New tests for H2HManager

--- peerbox/src/test/java/org/peerbox/model/H2HManagerTest.java
+++ peerbox/src/test/java/org/peerbox/model/H2HManagerTest.java
@@ -2,7 +2,11 @@ package org.peerbox.model;
 
 import static org.junit.Assert.*;
 
+import java.net.InetAddress;
 import java.net.UnknownHostException;
+
+import org.hive2hive.core.api.configs.NetworkConfiguration;
+import org.hive2hive.core.exceptions.NoPeerConnectionException;
 import org.junit.Test;
 
 public class H2HManagerTest {
@@ -24,5 +28,31 @@ public class H2HManagerTest {
 		H2HManager.INSTANCE.createNode();
 		assertTrue(H2HManager.INSTANCE.accessNetwork("localhost"));
 	}
-
+	
+	@Test
+	public void getInetAddressAsStringIfInitialPeer(){
+		try {
+			H2HManager.INSTANCE.createNode();
+			String address = H2HManager.INSTANCE.getInetAddressAsString();
+			System.out.println(address);
+			String localhost = InetAddress.getLocalHost().getHostAddress();
+			assertEquals(address, localhost);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	@Test
+	public void getInetAddressAsStringIfNotInitialPeer(){
+		String nodeID = H2HManager.INSTANCE.generateNodeID();
+		String bootstrapAddress = "1.2.3.4";
+		try {
+			H2HManager.INSTANCE.createNode(NetworkConfiguration.create(nodeID, InetAddress.getByName(bootstrapAddress)));
+			String address = H2HManager.INSTANCE.getInetAddressAsString();
+			assertEquals(address, bootstrapAddress);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
