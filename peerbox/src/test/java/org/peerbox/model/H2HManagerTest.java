b037fc378aa042cfa688f99e0e8d9a75f4565e34
Andreas Albrecht <albrecht@iodir.com>
2014-07-20 01:05:18 +0200

    + Unit tests adapted (H2H Manager instance instead of singleton)

--- peerbox/src/test/java/org/peerbox/model/H2HManagerTest.java
+++ peerbox/src/test/java/org/peerbox/model/H2HManagerTest.java
@@ -16,35 +16,37 @@ import org.junit.Test;
 public class H2HManagerTest {
 	
 	private String path;
+	private H2HManager h2hManager;
 	
 	@Before
 	public void initializeVariables(){
 		path = System.getProperty("user.dir").replace("\\", "/") + "/"; 
+		h2hManager = new H2HManager();
 	}
 	
 	@Test(expected=UnknownHostException.class)
 	public void accessNetworkTestUnknownHost() throws UnknownHostException{
-		H2HManager.INSTANCE.createNode();
-		H2HManager.INSTANCE.accessNetwork("unknownhost");
+		h2hManager.createNode();
+		h2hManager.accessNetwork("unknownhost");
 	}
 	
 	@Test
 	public void accessNetworkTestWrongBootstrapAddress() throws UnknownHostException{
-		H2HManager.INSTANCE.createNode();
-		assertFalse(H2HManager.INSTANCE.accessNetwork("1.2.3.4"));
+		h2hManager.createNode();
+		assertFalse(h2hManager.accessNetwork("1.2.3.4"));
 	}
 	
 	@Test
 	public void accessNetworkTestCorrectBootstrapAddress() throws UnknownHostException{
-		H2HManager.INSTANCE.createNode();
-		assertTrue(H2HManager.INSTANCE.accessNetwork("localhost"));
+		h2hManager.createNode();
+		assertTrue(h2hManager.accessNetwork("localhost"));
 	}
 	
 	@Test
 	public void getInetAddressAsStringIfInitialPeer(){
 		try {
-			H2HManager.INSTANCE.createNode();
-			String address = H2HManager.INSTANCE.getInetAddressAsString();
+			h2hManager.createNode();
+			String address = h2hManager.getInetAddressAsString();
 			System.out.println(address);
 			String localhost = InetAddress.getLocalHost().getHostAddress();
 			assertEquals(address, localhost);
@@ -55,11 +57,11 @@ public class H2HManagerTest {
 	}
 	@Test
 	public void getInetAddressAsStringIfNotInitialPeer(){
-		String nodeID = H2HManager.INSTANCE.generateNodeID();
+		String nodeID = h2hManager.generateNodeID();
 		String bootstrapAddress = "1.2.3.4";
 		try {
-			H2HManager.INSTANCE.createNode(NetworkConfiguration.create(nodeID, InetAddress.getByName(bootstrapAddress)));
-			String address = H2HManager.INSTANCE.getInetAddressAsString();
+			h2hManager.createNode(NetworkConfiguration.create(nodeID, InetAddress.getByName(bootstrapAddress)));
+			String address = h2hManager.getInetAddressAsString();
 			assertEquals(address, bootstrapAddress);
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
@@ -71,7 +73,7 @@ public class H2HManagerTest {
 	public void initializeRootDirectoryButSelectFile() throws IOException{
 		File testFile = new File("Test.txt");
 		testFile.createNewFile();
-		H2HManager.INSTANCE.initializeRootDirectory("Test.txt");
+		h2hManager.initializeRootDirectory("Test.txt");
 		
 	}
 	
@@ -79,13 +81,13 @@ public class H2HManagerTest {
 	public void initializeRootDirectoryWithExistingDirectory() throws IOException{
 		File testDir = new File("Test_ExistingDir");
 		testDir.mkdir();
-		H2HManager.INSTANCE.initializeRootDirectory("Test_ExistingDir");
+		h2hManager.initializeRootDirectory("Test_ExistingDir");
 		testDir.delete();
 	}
 	
 	@Test
 	public void initializeRootDirectoryWithNewDirectory() throws IOException{
-		boolean isSuccessfull = H2HManager.INSTANCE.initializeRootDirectory(path + "Test_NewDir");
+		boolean isSuccessfull = h2hManager.initializeRootDirectory(path + "Test_NewDir");
 		assertTrue(isSuccessfull);
 		File newDir = new File(path + "Test_NewDir");
 		assertTrue(newDir.exists());
@@ -95,7 +97,7 @@ public class H2HManagerTest {
 	
 	@Test
 	public void initializeRootDirectoryWithoutParentDir() throws IOException{
-		boolean isSuccessfull = H2HManager.INSTANCE.initializeRootDirectory(path + "doesnotexist/Test_Dir");
+		boolean isSuccessfull = h2hManager.initializeRootDirectory(path + "doesnotexist/Test_Dir");
 		assertFalse(isSuccessfull);
 	}
 }
--- peerbox/src/test/java/org/peerbox/presenter/CreateNetworkControllerTest.java
+++ peerbox/src/test/java/org/peerbox/presenter/CreateNetworkControllerTest.java
@@ -1,21 +1,24 @@
 package org.peerbox.presenter;
 
-import java.net.URL;
 
-import javafx.event.ActionEvent;
-
+import org.junit.Before;
 import org.junit.Test;
 import org.peerbox.model.H2HManager;
 import org.peerbox.presenter.CreateNetworkController;
 
-import junit.framework.TestCase;
-
 public class CreateNetworkControllerTest {
-
+	
+	private H2HManager h2hmanager;
+	
+	@Before
+	public void initializeVariables(){
+		h2hmanager = new H2HManager();
+	}
+	
 	@Test
 	public void createNetworkTest(){
-		CreateNetworkController controller = new CreateNetworkController();
-		assert(H2HManager.INSTANCE.getNode() != null);
-		
+		CreateNetworkController controller = new CreateNetworkController(h2hmanager);
+		assert(h2hmanager.getNode() != null);
+		// TODO: why should the node be != null after constructor?
 	}
 }
