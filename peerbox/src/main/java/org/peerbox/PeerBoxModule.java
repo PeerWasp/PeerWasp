dc5fd798d673f7b4f4246da0b97dabe693eef636
Andreas Albrecht <albrecht@iodir.com>
2014-07-19 21:53:38 +0200

    + added Guice bindings for the controllers

--- peerbox/src/main/java/org/peerbox/PeerBoxModule.java
+++ peerbox/src/main/java/org/peerbox/PeerBoxModule.java
@@ -1,6 +1,12 @@
 package org.peerbox;
 
+import org.peerbox.model.H2HManager;
+import org.peerbox.presenter.CreateNetworkController;
+import org.peerbox.presenter.JoinNetworkController;
+import org.peerbox.presenter.LoginController;
 import org.peerbox.presenter.NetworkSelectionController;
+import org.peerbox.presenter.RegisterController;
+import org.peerbox.presenter.SelectRootPathController;
 
 import com.google.inject.AbstractModule;
 
@@ -8,7 +14,13 @@ public class PeerBoxModule extends AbstractModule {
 
 	@Override
 	protected void configure() {
+		bind(H2HManager.class);
+		bind(CreateNetworkController.class);
+		bind(JoinNetworkController.class);
+		bind(LoginController.class);
 		bind(NetworkSelectionController.class);
+		bind(RegisterController.class);
+		bind(SelectRootPathController.class);
 	}
 
 }
