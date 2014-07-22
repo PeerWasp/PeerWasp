3b0de0b82ce2db2a4c733881d3a61015dc3669ea
patwinz <pwinzenried@gmail.com>
2014-07-22 10:41:00 +0200

    + minimize action implemented

--- peerbox/src/main/java/org/peerbox/presenter/MainController.java
+++ peerbox/src/main/java/org/peerbox/presenter/MainController.java
@@ -2,14 +2,20 @@ package org.peerbox.presenter;
 
 import org.peerbox.interfaces.INavigatable;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class MainController implements INavigatable {
 
 	@FXML
 	private Pane mainPane;
+	
+	@FXML
+	private VBox mainBox;
 
 	/* (non-Javadoc)
 	 * @see org.peerbox.presenter.INavigatable#setContent(javafx.scene.Node)
@@ -21,13 +27,15 @@ public class MainController implements INavigatable {
 		mainPane.requestLayout();
 	}
 	
-	public void closeApp(){
+	public void closeApp(ActionEvent event){
 		System.out.println("Application closed.");
 		System.exit(0);
 	}
 	
-	public void minApp(){
-		System.out.println("Application minimized (not yet implemented).");
-		//TODO
+	public void minApp(ActionEvent event){
+		System.out.println("Application minimized.");
+		Stage stage = (Stage) mainBox.getScene().getWindow();
+		stage.setIconified(true);
+		
 	}
 }
