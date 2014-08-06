cfa194cbc69275b9932ac0f887b42016b3c9140f
Andreas Albrecht <albrecht@iodir.com>
2014-08-06 13:34:54 +0200

    + Moved dragging logic from App into MainView

--- peerbox/src/main/java/org/peerbox/presenter/MainController.java
+++ peerbox/src/main/java/org/peerbox/presenter/MainController.java
@@ -4,8 +4,10 @@ import org.peerbox.interfaces.INavigatable;
 
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -14,6 +16,10 @@ public class MainController implements INavigatable {
 
 	@FXML
 	private Pane mainPane;
+	
+	//offset values are needed for calculating position of application
+	private double xOffset = 0;
+	private double yOffset = 0;
 
 	/* (non-Javadoc)
 	 * @see org.peerbox.presenter.INavigatable#setContent(javafx.scene.Node)
@@ -35,5 +41,18 @@ public class MainController implements INavigatable {
 		System.out.println("Application minimized.");
 		Stage stage = (Stage) mainPane.getScene().getWindow();
 		stage.setIconified(true);
+	}
+	
+	// provides ability to move application window wherever the user clicks & drags with the mouse
+	public void onMousePressed(MouseEvent event) {
+		xOffset = event.getSceneX();
+		yOffset = event.getSceneY();
+	}
+
+	// releases window at position when mouse click is released
+	public void onMouseDragged(MouseEvent event) {
+		Stage stage = (Stage) mainPane.getScene().getWindow();
+		stage.setX(event.getScreenX() - xOffset);
+		stage.setY(event.getScreenY() - yOffset);
 	}
 }
