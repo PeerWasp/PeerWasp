0e787600fe1e4ad455b5061482c4c93876b76805
Andreas Albrecht <albrecht@iodir.com>
2014-08-07 23:46:20 +0200

    + Settings views

---
+++ peerbox/src/main/java/org/peerbox/presenter/settings/Account.java
@@ -0,0 +1,16 @@
+package org.peerbox.presenter.settings;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class Account implements Initializable {
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
