30da96fc9c2b6831b92fa04c4de83343b9b2eaa2
Andreas Albrecht <albrecht@iodir.com>
2014-08-09 17:53:37 +0200

    + refactoring of error label

--- peerbox/src/main/java/org/peerbox/view/controls/ErrorLabel.java
+++ peerbox/src/main/java/org/peerbox/view/controls/ErrorLabel.java
@@ -1,37 +1,26 @@
 package org.peerbox.view.controls;
 
-import java.io.IOException;
-import java.net.URL;
-
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
 
-public class ErrorLabel extends HBox {
+public class ErrorLabel extends Label {
 
 	@FXML
 	private ImageView imgView;
 	private DoubleProperty iconHeightProperty;
-	@FXML
-	private Label lblMessage;
 
 	public ErrorLabel() {
-		try {
-			URL icon = getClass().getResource("/org/peerbox/view/controls/error_label.fxml");
-			FXMLLoader fxmlLoader = new FXMLLoader(icon);
-			fxmlLoader.setRoot(this);
-			fxmlLoader.setController(this);
-			fxmlLoader.load();
-			imgView.fitHeightProperty().bind(iconHeightProperty());
-			visibleProperty().bind(lblMessage.textProperty().isNotEmpty());
-		} catch (IOException exception) {
-			throw new RuntimeException(exception);
-		}
+		imgView = new ImageView(new Image(ErrorLabel.class.getResourceAsStream("/images/error-icon.png")));
+		imgView.setPreserveRatio(true);
+		imgView.setSmooth(true);
+		imgView.fitHeightProperty().bind(iconHeightProperty());
+		setGraphic(imgView);
+		visibleProperty().bind(textProperty().isNotEmpty());
+		setWrapText(true);
 	}
 
 	public DoubleProperty iconHeightProperty() {
@@ -49,15 +38,4 @@ public class ErrorLabel extends HBox {
 		return iconHeightProperty().get();
 	}
 
-	public String getText() {
-		return textProperty().get();
-	}
-
-	public void setText(String value) {
-		textProperty().set(value);
-	}
-
-	public StringProperty textProperty() {
-		return lblMessage.textProperty();
-	}
 }
