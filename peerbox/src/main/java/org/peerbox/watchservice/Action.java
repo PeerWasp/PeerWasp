3cc078d8255f29cf80a5c58f1ef37058b561c88a
patwinz <pwinzenried@gmail.com>
2014-09-03 22:07:34 +0200

    ~ javadoc, minor readability changes

--- peerbox/src/main/java/org/peerbox/watchservice/Action.java
+++ peerbox/src/main/java/org/peerbox/watchservice/Action.java
@@ -9,6 +9,15 @@ import org.hive2hive.core.exceptions.NoSessionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * The Action class provides a systematic and lose-coupled way to change the 
+ * state of an object as part of the chosen state pattern design.
+ * 
+ * 
+ * @author albrecht, anliker, winzenried
+ *
+ */
+
 public class Action {
 	
 	private final static Logger logger = LoggerFactory.getLogger(Action.class);
@@ -18,7 +27,10 @@ public class Action {
 	private File file;
 	
 	private ActionState currentState;
-		
+	
+	/**
+	 * Initialize with timestamp and set currentState to initial state
+	 */
 	public Action(){
 		timestamp = Calendar.getInstance().getTimeInMillis(); 
 		currentState = new InitialState();
@@ -37,19 +49,34 @@ public class Action {
 		this.timestamp = timestamp;
 	}
 	
-	
+	/**
+	 * changes the state of the currentState to Create state if current state allows it.
+	 */
 	public void handleCreateEvent(){
 		currentState = currentState.handleCreateEvent();
 	}
 	
+	/**
+	 * changes the state of the currentState to Delete state if current state allows it.
+	 */
 	public void handleDeleteEvent(){
 		currentState = currentState.handleDeleteEvent();
 	}
 	
+	/**
+	 * changes the state of the currentState to Modify state if current state allows it.
+	 */
 	public void handleModifyEvent(){
 		currentState = currentState.handleModifyEvent();
 	}
 	
+	/**
+	 * Each state is able to execute an action as soon the state is considered as stable. 
+	 * The action itself depends on the current state (e.g. add file, delete file, etc.)
+	 * @throws NoSessionException
+	 * @throws NoPeerConnectionException
+	 * @throws IllegalFileLocation
+	 */
 	//execute action depending on state
 	public void execute() throws NoSessionException, NoPeerConnectionException, IllegalFileLocation{
 		logger.debug("Execute action...");
@@ -64,6 +91,9 @@ public class Action {
 		return timestamp;
 	}
 	
+	/**
+	 * @return current state object
+	 */
 	public ActionState getCurrentState(){	
 			if (currentState.getClass() == CreateState.class){
 				logger.debug("Current State: Create");
