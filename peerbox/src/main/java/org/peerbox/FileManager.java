c0a262d7e3bab50194ac479b0335e8083c32ef65
Andreas Albrecht <albrecht@iodir.com>
2014-09-27 19:26:26 +0200

    + file manager: add/delete/update calls to H2H file manager

--- peerbox/src/main/java/org/peerbox/FileManager.java
+++ peerbox/src/main/java/org/peerbox/FileManager.java
@@ -4,41 +4,56 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.List;
 
+import org.hive2hive.core.api.interfaces.IFileManager;
 import org.hive2hive.core.exceptions.IllegalFileLocation;
 import org.hive2hive.core.exceptions.NoPeerConnectionException;
 import org.hive2hive.core.exceptions.NoSessionException;
 import org.hive2hive.core.model.PermissionType;
 import org.hive2hive.core.processes.files.list.FileTaste;
 import org.hive2hive.core.processes.files.recover.IVersionSelector;
+import org.hive2hive.processframework.exceptions.InvalidProcessStateException;
 import org.hive2hive.processframework.interfaces.IProcessComponent;
 import org.hive2hive.processframework.interfaces.IResultProcessComponent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FileManager {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(FileManager.class);
+	private IFileManager h2hFileManager;
+
+	public FileManager(IFileManager h2hFileManager) {
+		this.h2hFileManager = h2hFileManager;
+	}
 
 	public IProcessComponent add(File file) throws NoSessionException, NoPeerConnectionException,
-			IllegalFileLocation {
+			IllegalFileLocation, InvalidProcessStateException {
 		logger.debug("ADD - {}", file);
-		return null;
+		IProcessComponent component = h2hFileManager.add(file);
+		component.start();
+		return component;
 	}
 
 	public IProcessComponent update(File file) throws NoSessionException, IllegalArgumentException,
-			NoPeerConnectionException {
+			NoPeerConnectionException, InvalidProcessStateException {
 		logger.debug("UPDATE - {}", file);
-		return null;
+		IProcessComponent component = h2hFileManager.update(file);
+		component.start();
+		return component;
+	}
+
+	public IProcessComponent delete(File file) throws NoSessionException,
+			NoPeerConnectionException, InvalidProcessStateException {
+		logger.debug("DELETE - {}", file);
+		IProcessComponent component = h2hFileManager.delete(file);
+		component.start();
+		return component;
 	}
 
 	public IProcessComponent move(File source, File destination) throws NoSessionException,
 			NoPeerConnectionException {
 		logger.debug("MOVE - from: {}, to: {}", source, destination);
-		return null;
-	}
-
-	public IProcessComponent delete(File file) throws NoSessionException, NoPeerConnectionException {
-		logger.debug("DELETE - {}", file);
+		// TODO: implement move.
 		return null;
 	}
 
