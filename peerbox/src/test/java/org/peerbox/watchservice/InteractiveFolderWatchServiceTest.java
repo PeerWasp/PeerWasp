1ec0b4d919b8d932ba4fa0388d4308e9bdd81cdf
Andreas Albrecht <albrecht@iodir.com>
2014-09-30 12:30:39 +0200

    + added simple listener implementation that only prints the events of the java watchservice

--- peerbox/src/test/java/org/peerbox/watchservice/InteractiveFolderWatchServiceTest.java
+++ peerbox/src/test/java/org/peerbox/watchservice/InteractiveFolderWatchServiceTest.java
@@ -3,6 +3,7 @@ package org.peerbox.watchservice;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -18,12 +19,16 @@ public class InteractiveFolderWatchServiceTest {
 	
 	public static void main(String[] args) throws Exception {
 		Path path = Paths.get(FileUtils.getTempDirectoryPath(), "PeerBox_FolderWatchServiceTest");
+		if(!Files.exists(path)) {
+			Files.createDirectories(path);
+		}
 		logger.info("Path: {}", path.toString());
 		
 		FolderWatchService service = new FolderWatchService(path);
-		FileEventManager eventManager = new FileEventManager(path);
-		eventManager.setFileManager(new FileManager());
-		service.addFileEventListener(eventManager);
+		service.addFileEventListener(new PrintListener());
+//		FileEventManager eventManager = new FileEventManager(path);
+//		eventManager.setFileManager(new FileManager(null));
+//		service.addFileEventListener(eventManager);
 		service.start();
 		
 		logger.info("Running");
@@ -31,5 +36,24 @@ public class InteractiveFolderWatchServiceTest {
 		// Thread.sleep(1000*10);
 		// service.stop();
 		// System.out.println("Stopping");
+	}
+	
+	
+	
+	private static class PrintListener implements ILocalFileEventListener {
+		@Override
+		public void onLocalFileModified(Path path) {
+			logger.info("onLocalFileModified: {}", path);
+		}
+		
+		@Override
+		public void onLocalFileDeleted(Path path) {
+			logger.info("onLocalFileDeleted: {}", path);
+		}
+		
+		@Override
+		public void onLocalFileCreated(Path path, boolean useFileWalker) {
+			logger.info("onLocalFileCreated: {}", path);
+		}
 	}
 }
