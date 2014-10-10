ea2173fe516fd0aca04ddf7cba97b42cf3b59541
patwinz <pwinzenried@gmail.com>
2014-10-10 09:49:03 +0200

    + current state of ConflictTest (no conflicting state yet...)

--- peerbox/src/test/java/org/peerbox/watchservice/integration/ConflictTest.java
+++ peerbox/src/test/java/org/peerbox/watchservice/integration/ConflictTest.java
@@ -15,20 +15,38 @@ import java.nio.channels.FileChannel;
 import java.nio.channels.FileLock;
 import java.nio.channels.OverlappingFileLockException;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import org.peerbox.utils.FileTestUtils;
 
 public class ConflictTest extends FileIntegrationTest{
 	
 	@Test
-	public void singleFileTest() throws IOException {
+	public void singleFileTest() throws IOException, InterruptedException {
+		
+		String homeDir = System.getProperty("user.home");
 		// ADD
 		Path file_1 = addSingleFile();
-		Path file_2 = addSingleFile();
+		
+		Path pathUser1 = Paths.get(homeDir + File.separator + "PeerBox_Test" + File.separator + "client-0" + File.separator + "test.txt");
+		Path pathUser2 = Paths.get(homeDir + File.separator + "PeerBox_Test" + File.separator + "client-1" + File.separator + "test.txt");
+		
+		FileUtils.writeStringToFile(pathUser1.toFile(), "ABCDEFGHIJ");
+		FileUtils.writeStringToFile(pathUser2.toFile(), "0123456789");
+		
+		System.out.println("FILE CONTENT USER 1: " + FileUtils.readFileToString(pathUser1.toFile()));
+		System.out.println("FILE CONTENT USER 2: " + FileUtils.readFileToString(pathUser2.toFile()));
+		
+		Thread.sleep(10000);
+		assertSyncClientPaths();
+		
+		System.out.println("FILE CONTENT USER 1 AFTER SYNC: " + FileUtils.readFileToString(pathUser1.toFile()));
+		System.out.println("FILE CONTENT USER 2 AFTER SYNC: " + FileUtils.readFileToString(pathUser2.toFile()));
+
 	}
 	
 	protected Path addSingleFile() throws IOException {
-		Path file = FileTestUtils.createTestFile(client.getRootPath(), NUMBER_OF_CHARS);
+		Path file = FileTestUtils.createTestFile(masterRootPath, NUMBER_OF_CHARS);
 		
 		waitForExists(file, WAIT_TIME_SHORT);
 		assertSyncClientPaths();
