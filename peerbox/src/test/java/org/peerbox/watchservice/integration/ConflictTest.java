cb3270f7ba19df79320e3cf4e54d81685b280fee
patwinz <pwinzenried@gmail.com>
2014-10-20 10:11:26 +0200

    ~ slightly adapted

--- peerbox/src/test/java/org/peerbox/watchservice/integration/ConflictTest.java
+++ peerbox/src/test/java/org/peerbox/watchservice/integration/ConflictTest.java
@@ -22,35 +22,44 @@ import org.peerbox.utils.FileTestUtils;
 public class ConflictTest extends FileIntegrationTest{
 	
 	@Test
-	public void singleFileTest() throws IOException, InterruptedException {
+	public void CreateCreateTest() throws IOException, InterruptedException {
 		
 		String homeDir = System.getProperty("user.home");
 		// ADD
 		//Path file_1 = addSingleFile();
 		
-		Path pathUser1 = Paths.get(homeDir + File.separator + "PeerBox_Test" + File.separator + "client-0" + File.separator + "test.txt");
-		Path pathUser2 = Paths.get(homeDir + File.separator + "PeerBox_Test" + File.separator + "client-1" + File.separator + "test.txt");
+		Path pathUser0 = Paths.get(homeDir + File.separator + "PeerBox_Test" + File.separator + "client-0" + File.separator + "test.txt");
+		Path pathUser1 = Paths.get(homeDir + File.separator + "PeerBox_Test" + File.separator + "client-1" + File.separator + "test.txt");
 		
-		FileUtils.writeStringToFile(pathUser1.toFile(), "ABCDEFGHIJ");
+		FileUtils.writeStringToFile(pathUser0.toFile(), "ABCDEFGHIJ");
 		
 		Thread.sleep(2000);
-		FileUtils.writeStringToFile(pathUser2.toFile(), "0123456789");
+		FileUtils.writeStringToFile(pathUser1.toFile(), "0123456789");
 		
-		System.out.println("FILE CONTENT USER 1: " + FileUtils.readFileToString(pathUser1.toFile()));
-		System.out.println("FILE CONTENT USER 2: " + FileUtils.readFileToString(pathUser2.toFile()));
+		System.out.println("FILE CONTENT USER 1: " + FileUtils.readFileToString(pathUser0.toFile()));
+		System.out.println("FILE CONTENT USER 2: " + FileUtils.readFileToString(pathUser1.toFile()));
 		Thread.sleep(10000);
 		assertSyncClientPaths();
 		
-		System.out.println("FILE CONTENT USER 1 AFTER SYNC: " + FileUtils.readFileToString(pathUser1.toFile()));
-		System.out.println("FILE CONTENT USER 2 AFTER SYNC: " + FileUtils.readFileToString(pathUser2.toFile()));
+		System.out.println("FILE CONTENT USER 1 AFTER SYNC: " + FileUtils.readFileToString(pathUser0.toFile()));
+		System.out.println("FILE CONTENT USER 2 AFTER SYNC: " + FileUtils.readFileToString(pathUser1.toFile()));
 
 	}
 	
-//	protected Path addSingleFile() throws IOException {
-//		Path file = FileTestUtils.createTestFile(masterRootPath, NUMBER_OF_CHARS);
-//		
-//		waitForExists(file, WAIT_TIME_SHORT);
-//		assertSyncClientPaths();
-//		return file;
-//	}
+	@Test
+	public void DeleteUpdateTest() throws IOException, InterruptedException {
+		
+		String homeDir = System.getProperty("user.home");
+		
+		Path pathUser0 = Paths.get(homeDir + File.separator + "PeerBox_Test" + File.separator + "client-0" + File.separator + "test.txt");
+		Path pathUser1 = Paths.get(homeDir + File.separator + "PeerBox_Test" + File.separator + "client-1" + File.separator + "test.txt");
+		
+		FileUtils.writeStringToFile(pathUser0.toFile(), "ABCDEFGHIJ");
+		FileUtils.writeStringToFile(pathUser1.toFile(), "ABCDEFGHIJ");
+		Thread.sleep(2000);
+		FileUtils.forceDelete(pathUser1.toFile());
+		Thread.sleep(10000);
+		//assertSyncClientPaths();
+
+	}
 }
