f1ad3b851cfe75e77ac65b660ba7f568c929bc8b
patwinz <pwinzenried@gmail.com>
2014-12-01 10:41:41 +0100

    + Rename test (not yet successful)

--- peerbox/src/test/java/org/peerbox/watchservice/integration/Rename.java
+++ peerbox/src/test/java/org/peerbox/watchservice/integration/Rename.java
@@ -1,5 +1,43 @@
 package org.peerbox.watchservice.integration;
 
-public class Rename {
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+import org.apache.commons.io.FilenameUtils;
+import org.junit.Test;
+import org.peerbox.utils.FileTestUtils;
+
+
+public class Rename extends FileIntegrationTest{
+	
+	/**
+	 * These tests only consider file and folder contents. The difference 
+	 * between delete/create and move is not tested yet!
+	 * @throws IOException
+	 * @throws InterruptedException 
+	 */	
+	@Test
+	public void singleFileRenameTest() throws IOException, InterruptedException{
+		Path initialFile = FileTestUtils.createRandomFile(masterRootPath, NUMBER_OF_CHARS);
+		waitForExists(initialFile, WAIT_TIME_SHORT);
+		assertSyncClientPaths();
+		
+		Path renamedFile = rename(initialFile, "RENAMED.file");
+		initialFile = renamedFile;
+		
+		
+		assertSyncClientPaths();
+	}
+	
+
+	Path rename(Path oldName, String newNameString) throws IOException{
+	    return Files.move(oldName, oldName.resolveSibling(newNameString));
+	}
+	
+
+	
+	
 }
