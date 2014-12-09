5c2bf135198516c8512af8fe4ea20e9e562ef700
Andreas Albrecht <albrecht@iodir.com>
2014-12-09 10:33:36 +0100

    bootstrap node: update to H2H changes

--- bootstrap-node/src/main/java/org/peerbox/bootstrapnode/Starter.java
+++ bootstrap-node/src/main/java/org/peerbox/bootstrapnode/Starter.java
@@ -1,11 +1,7 @@
 package org.peerbox.bootstrapnode;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.UUID;
 
 import org.hive2hive.core.api.H2HNode;
@@ -14,19 +10,12 @@ import org.hive2hive.core.api.configs.NetworkConfiguration;
 import org.hive2hive.core.api.interfaces.IFileConfiguration;
 import org.hive2hive.core.api.interfaces.IH2HNode;
 import org.hive2hive.core.api.interfaces.INetworkConfiguration;
-import org.hive2hive.core.api.interfaces.IUserManager;
-import org.hive2hive.core.exceptions.NoPeerConnectionException;
-import org.hive2hive.core.file.IFileAgent;
-import org.hive2hive.core.security.UserCredentials;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Starter {
 	
-	private static final String PIN = "123";
-	private static final String PASSWORD = "123456";
-	private static final String USER = "user";
-	private static final String ROOT_DIRECTORY = "/tmp/peerbox";
-
-
+	private static final Logger logger = LoggerFactory.getLogger(Starter.class);
 
 	public static void main(String[] args) {
 		try {
@@ -35,73 +24,27 @@ public class Starter {
 			
 			if(args.length == 0) {
 				// initial peer
-				System.out.println("Initial peer");
+				logger.info("Start as initial peer");
 				netConfig = NetworkConfiguration.createInitial();
 			} else if(args.length >= 1) {
 				// bootstrap to node
-				System.out.println("Connect to other node");
+				logger.info("Connect to existing peer");
 				InetAddress address = InetAddress.getByName(args[0]);
 				netConfig = NetworkConfiguration.create(UUID.randomUUID().toString(),  address);
 			}
 		
 			IFileConfiguration fileConfig = FileConfiguration.createDefault();
-			IH2HNode peerNode = H2HNode.createNode(netConfig, fileConfig);
-			peerNode.connect();
-			
-			if(args.length == 2) {
-				// register if not exists and login
-				registerAndLogin(peerNode);
+			IH2HNode peerNode = H2HNode.createNode(fileConfig);
+			boolean success = peerNode.connect(netConfig);
+			if(!success) {
+				logger.error("Could not connect to other node: {}", args[0]);
 			}
 		
 			// do not exit
 			Thread.currentThread().join();
 		
-		} catch (InterruptedException | NoPeerConnectionException | IOException e) {
-			System.err.println("Exception: " + e.getMessage());
-			e.printStackTrace();
-		}
-		
-	}
-
-	private static void registerAndLogin(IH2HNode peerNode) throws NoPeerConnectionException, InterruptedException, IOException {
-		IUserManager userManager = peerNode.getUserManager();
-
-		UserCredentials credentials = new UserCredentials(USER, PASSWORD, PIN);
-		Path rootDirectory = Paths.get(ROOT_DIRECTORY);
-		if(!Files.exists(rootDirectory)) {
-			Files.createDirectory(rootDirectory);
-		}
-
-		if (!userManager.isRegistered(credentials.getUserId())) {
-		    userManager.register(credentials).await();
-		}
-		userManager.login(credentials, new FileAgent(rootDirectory)).await();
-	}
-	
-	
-	
-	
-	private static class FileAgent implements IFileAgent {
-		private Path rootDirectory;
-		
-		public FileAgent(Path rootDirectory) {
-			this.rootDirectory = rootDirectory;
-		}
-
-		@Override
-		public File getRoot() {
-			return rootDirectory.toFile();
-		}
-
-		@Override
-		public void writeCache(String key, byte[] data) throws IOException {
-			// TODO Auto-generated method stub
-		}
-
-		@Override
-		public byte[] readCache(String key) throws IOException {
-			// TODO Auto-generated method stub
-			return null;
+		} catch (InterruptedException | IOException e) {
+			logger.warn("Exception: {}", e.getMessage(), e);
 		}
 		
 	}
