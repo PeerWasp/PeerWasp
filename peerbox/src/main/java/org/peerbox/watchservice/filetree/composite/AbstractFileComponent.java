cd2761bb5e61cf36bdfb6455605a5107a55398a1
raute <claudioanliker@hotmail.com>
2015-01-14 11:07:03 +0100

    Fix error in collection of synchronized files for Tray-GUI

--- peerbox/src/main/java/org/peerbox/watchservice/filetree/composite/AbstractFileComponent.java
+++ peerbox/src/main/java/org/peerbox/watchservice/filetree/composite/AbstractFileComponent.java
@@ -148,7 +148,7 @@ abstract class AbstractFileComponent implements FileComponent {
 				+ "This is probably a file. "
 						+ "(this=%s)", getPath());
 
-		throw new NotImplException(msg);
+//		throw new NotImplException(msg);
 	}
 
 }
