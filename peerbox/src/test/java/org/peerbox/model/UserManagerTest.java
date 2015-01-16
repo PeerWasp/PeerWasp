6a8a837eb77d8829643c787f363e17e9b80270c7
Andreas Albrecht <albrecht@iodir.com>
2015-01-16 16:34:20 +0100

    increase network size for usermanager test

--- peerbox/src/test/java/org/peerbox/model/UserManagerTest.java
+++ peerbox/src/test/java/org/peerbox/model/UserManagerTest.java
@@ -35,23 +35,23 @@ import org.peerbox.app.manager.user.UserManager;
 import org.peerbox.events.MessageBus;
 
 public class UserManagerTest {
-	private static final int networkSize = 6;
+	private static final int NETWORK_SIZE = 15;
 	private static List<IH2HNode> network;
 
 	private UserCredentials userCredentials;
 	private Path root;
 	private ClientContext client;
-	
+
 	@BeforeClass
 	public static void beforeClass() {
-		network = NetworkTestUtil.createH2HNetwork(networkSize);
+		network = NetworkTestUtil.createH2HNetwork(NETWORK_SIZE);
 	}
-	
+
 	@AfterClass
 	public static void afterClass() {
 		NetworkTestUtil.shutdownH2HNetwork(network);
 	}
-	
+
 	@Before
 	public void beforeTest() {
 		root = FileTestUtil.getTempDirectory().toPath();
@@ -61,7 +61,7 @@ public class UserManagerTest {
 		IH2HNode clientNode = network.get(RandomUtils.nextInt(0, network.size()));
 		client = createClientContext(clientNode);
 	}
-	
+
 	@Test
 	public void testRegister() throws NoPeerConnectionException {
 		// check NOT registered
@@ -70,18 +70,18 @@ public class UserManagerTest {
 			IUserManager usrMgr = cc.getUserManager();
 			assertFalse(usrMgr.isRegistered(userCredentials.getUserId()));
 		}
-		
+
 		// register user
 		ResultStatus res = client.getUserManager().registerUser(
 				userCredentials.getUserId(), userCredentials.getPassword(), userCredentials.getPin());
 		assertTrue(res.isOk());
-		
+
 		// got event?
 		ArgumentCaptor<RegisterMessage> event = ArgumentCaptor.forClass(RegisterMessage.class);
 		Mockito.verify(client.getMessageBus(), Mockito.times(1)).publish(event.capture());
 		assertNotNull(event.getValue());
 		assertEquals(userCredentials.getUserId(), event.getValue().getUsername());
-		
+
 		// check again whether registered
 		for(int i = 0; i < network.size(); ++i) {
 			ClientContext cc = createClientContext(network.get(i));
@@ -89,7 +89,7 @@ public class UserManagerTest {
 			assertTrue(usrMgr.isRegistered(userCredentials.getUserId()));
 		}
 	}
-	
+
 	@Test
 	public void testAlreadyRegistered() throws NoPeerConnectionException {
 
@@ -97,40 +97,40 @@ public class UserManagerTest {
 		ResultStatus res = client.getUserManager().registerUser(
 				userCredentials.getUserId(), userCredentials.getPassword(), userCredentials.getPin());
 		assertTrue(res.isOk());
-		
+
 		// got event?
 		Mockito.verify(client.getMessageBus(), Mockito.times(1)).publish(Mockito.anyObject());
-		
+
 		// try register -- should fail
 		for (int i = 0; i < network.size(); ++i) {
 			ClientContext cc = createClientContext(network.get(i));
 			IUserManager usrMgr = cc.getUserManager();
 			res = usrMgr.registerUser(userCredentials.getUserId(), userCredentials.getPassword(), userCredentials.getPin());
 			assertTrue(res.isError());
-			
+
 			// got NO event?
 			Mockito.verify(cc.getMessageBus(), Mockito.never()).publish(Mockito.anyObject());
 		}
 	}
-	
+
 	@Test
 	public void testLogin() throws NoPeerConnectionException, IOException {
 		IUserManager userManager = client.getUserManager();
 		// not loggedIn
 		assertFalse(userManager.isLoggedIn());
-		
+
 		// register
 		ResultStatus res = userManager.registerUser(userCredentials.getUserId(), userCredentials.getPassword(), userCredentials.getPin());
 		assertTrue(res.isOk());
 		assertFalse(userManager.isLoggedIn());
 		// got event?
 		Mockito.verify(client.getMessageBus(), Mockito.times(1)).publish(Mockito.any());
-		
+
 		// login and loggedIn
 		res = userManager.loginUser(userCredentials.getUserId(), userCredentials.getPassword(), userCredentials.getPin(), root);
 		assertTrue(res.isOk());
 		assertTrue(userManager.isLoggedIn());
-		
+
 		// got event?
 		ArgumentCaptor<LoginMessage> event = ArgumentCaptor.forClass(LoginMessage.class);
 		Mockito.verify(client.getMessageBus(), Mockito.times(2)).publish(event.capture());
@@ -141,32 +141,32 @@ public class UserManagerTest {
 	@Test
 	public void testNotRegisteredLogin() throws NoPeerConnectionException, IOException {
 		IUserManager userManager = client.getUserManager();
-		
+
 		// not registered
 		assertFalse(userManager.isRegistered(userCredentials.getUserId()));
-		
+
 		// login and not loggedIn
 		ResultStatus res = userManager.loginUser(userCredentials.getUserId(), userCredentials.getPassword(), userCredentials.getPin(), root);
 		assertTrue(res.isError());
 		assertFalse(userManager.isLoggedIn());
-		
+
 		// got NO event?
 		Mockito.verify(client.getMessageBus(), Mockito.never()).publish(Mockito.anyObject());
 	}
-	
+
 	@Test
 	public void testLoginWrongCredentials() throws NoPeerConnectionException, IOException {
 		IUserManager userManager = client.getUserManager();
 		// not loggedIn
 		assertFalse(userManager.isLoggedIn());
-		
+
 		// register
 		ResultStatus res = userManager.registerUser(userCredentials.getUserId(), userCredentials.getPassword(), userCredentials.getPin());
 		assertTrue(res.isOk());
 		assertFalse(userManager.isLoggedIn());
 		// got event?
 		Mockito.verify(client.getMessageBus(), Mockito.times(1)).publish(Mockito.any());
-		
+
 		// wrong credentials -- login and loggedIn
 		UserCredentials wrongCred = H2HJUnitTest.generateRandomCredentials();
 		res = userManager.loginUser(wrongCred.getUserId(), wrongCred.getPassword(), wrongCred.getPin(), root);
@@ -174,7 +174,7 @@ public class UserManagerTest {
 		assertFalse(userManager.isLoggedIn());
 		// got NO additional event?
 		Mockito.verify(client.getMessageBus(), Mockito.times(1)).publish(Mockito.any());
-				
+
 		// correct credentials -- login and loggedIn
 		res = userManager.loginUser(userCredentials.getUserId(), userCredentials.getPassword(), userCredentials.getPin(), root);
 		assertTrue(res.isOk());
@@ -185,39 +185,39 @@ public class UserManagerTest {
 		assertNotNull(event.getValue());
 		assertEquals(userCredentials.getUserId(), event.getValue().getUsername());
 	}
-	
+
 	@Test
 	public void testLogout() throws NoPeerConnectionException, IOException, NoSessionException {
 		// register and login
 		testLogin();
 		IUserManager userManager = client.getUserManager();
-		
+
 		assertTrue(userManager.isLoggedIn());
 		ResultStatus res = userManager.logoutUser();
 		assertTrue(res.isOk());
 		assertFalse(userManager.isLoggedIn());
-		
+
 		// got event? (Register, login, logout)
 		ArgumentCaptor<LogoutMessage> event = ArgumentCaptor.forClass(LogoutMessage.class);
 		Mockito.verify(client.getMessageBus(), Mockito.times(3)).publish(event.capture());
 		assertNotNull(event.getValue());
 		assertEquals(userCredentials.getUserId(), event.getValue().getUsername());
 	}
-	
+
 	@Test(expected=NoSessionException.class)
 	public void testLogoutNotLoggedIn() throws NoPeerConnectionException, NoSessionException {
 		IUserManager userManager = client.getUserManager();
 		assertFalse(userManager.isLoggedIn());
-		
+
 		ResultStatus res = userManager.logoutUser();
 		assertFalse(res.isOk());
 		fail("NoSessionException not thrown.");
 	}
-	
+
 	private ClientContext createClientContext(IH2HNode node) {
 		return new ClientContext(node);
 	}
-	
+
 	private class ClientContext {
 		private IH2HNode node;
 		private INodeManager nodeManager;
