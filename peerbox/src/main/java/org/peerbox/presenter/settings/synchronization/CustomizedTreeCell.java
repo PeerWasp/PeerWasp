fa6c5701c69757d8ca610855a389026b0e17a76d
raute <claudioanliker@hotmail.com>
2015-02-13 10:51:58 +0100

    commit of new class

---
+++ peerbox/src/main/java/org/peerbox/presenter/settings/synchronization/CustomizedTreeCell.java
@@ -0,0 +1,40 @@
+package org.peerbox.presenter.settings.synchronization;
+
+import java.nio.file.Path;
+
+import org.peerbox.watchservice.FileEventManager;
+import org.peerbox.watchservice.IFileEventManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.Tooltip;
+import javafx.scene.control.cell.CheckBoxTreeCell;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.control.MenuItem;
+public class CustomizedTreeCell extends CheckBoxTreeCell<PathItem> {
+	
+	private static final Logger logger = LoggerFactory.getLogger(CustomizedTreeCell.class);
+	private ContextMenu menu = new ContextMenu();
+	private PathTreeItem item;
+	
+	public CustomizedTreeCell(IFileEventManager fileEventManager){
+		
+		MenuItem deleteItem = new MenuItem("Delete from network");
+		menu.getItems().add(deleteItem);
+		deleteItem.setOnAction(new EventHandler() {
+			public void handle(Event t) {
+				fileEventManager.onLocalFileHardDelete(getItem().getPath());
+			}
+		});
+		
+		final Tooltip tooltip = new Tooltip();
+        tooltip.setText(
+            "\nThis is an example tooltip\n");
+	  
+        setContextMenu(menu);
+        setTooltip(tooltip);
+	}
+
+}
