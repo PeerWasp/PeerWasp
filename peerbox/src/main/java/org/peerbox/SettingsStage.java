c00e40bb984ca65ec95d96b1485aa3eaee6401e1
Andreas Albrecht <albrecht@iodir.com>
2015-02-18 13:01:17 +0100

    fixing settings stage (loading settings)

--- peerbox/src/main/java/org/peerbox/SettingsStage.java
+++ peerbox/src/main/java/org/peerbox/SettingsStage.java
@@ -10,11 +10,13 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
+import org.peerbox.app.AppContext;
 import org.peerbox.interfaces.IFxmlLoaderProvider;
 import org.peerbox.view.ViewNames;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Preconditions;
 import com.google.inject.Inject;
 
 public class SettingsStage {
@@ -27,7 +29,13 @@ public class SettingsStage {
 	private static final double WINDOW_HEIGHT = 450.0;
 
 	private IFxmlLoaderProvider fxmlLoaderProvider;
+	private AppContext appContext;
 	private Stage stage;
+
+	@Inject
+	public SettingsStage(AppContext appContext) {
+		this.appContext = appContext;
+	}
 
 	@Inject
 	public void setFxmlLoaderProvider(IFxmlLoaderProvider fxmlLoaderProvider) {
@@ -36,8 +44,10 @@ public class SettingsStage {
 
 	private void load() {
 		try {
+			Preconditions.checkNotNull(appContext.getCurrentClientContext(), "ClientContext must not be null.");
 
-			FXMLLoader loader = fxmlLoaderProvider.create(ViewNames.SETTINGS_MAIN);
+			// important: use injector for client here because of client specific instances.
+			FXMLLoader loader = fxmlLoaderProvider.create(ViewNames.SETTINGS_MAIN, appContext.getCurrentClientContext().getInjector());
 			Parent root = loader.load();
 			Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
 			stage = new Stage();
--- peerbox/src/main/java/org/peerbox/presenter/settings/Settings.java
+++ peerbox/src/main/java/org/peerbox/presenter/settings/Settings.java
@@ -3,21 +3,25 @@ package org.peerbox.presenter.settings;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import org.peerbox.presenter.settings.synchronization.Synchronization;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 public class Settings implements Initializable {
 
-	@FXML 
+	@FXML
 	private Account accountController;
-	
-	@FXML 
+
+	@FXML
 	private Network networkController;
-	
+
+	@FXML
+	private Synchronization synchronizationController;
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
+
 	}
 
-	
 }
