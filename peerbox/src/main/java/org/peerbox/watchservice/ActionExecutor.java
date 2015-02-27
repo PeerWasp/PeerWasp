404a107779594d1b7b4b9cb235b901ead028a219
raute <claudioanliker@hotmail.com>
2015-02-27 18:09:37 +0100

    Action-waittime set to 5 seconds in actionexecutor

--- peerbox/src/main/java/org/peerbox/watchservice/ActionExecutor.java
+++ peerbox/src/main/java/org/peerbox/watchservice/ActionExecutor.java
@@ -54,7 +54,7 @@ public class ActionExecutor implements Runnable {
 
 
 	/** Amount of time that an action has to be "stable" in order to be executed **/
-	public static final long ACTION_WAIT_TIME_MS = 1000;
+	public static final long ACTION_WAIT_TIME_MS = 5000;
 	public static final int ACTION_WAIT_TIME_SEC = (int)(ACTION_WAIT_TIME_MS / 1000);
 	
 	/** Maximal number of concurrent network transactions **/
