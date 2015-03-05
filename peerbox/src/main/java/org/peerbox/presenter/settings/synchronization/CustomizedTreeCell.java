9c6d00fff841e42b5f4146d34eef8eb8474aadad
raute <claudioanliker@hotmail.com>
2015-03-05 12:05:53 +0100

    + CustomizedTreeCell: Properties-tab in context menu

--- peerbox/src/main/java/org/peerbox/presenter/settings/synchronization/CustomizedTreeCell.java
+++ peerbox/src/main/java/org/peerbox/presenter/settings/synchronization/CustomizedTreeCell.java
@@ -3,33 +3,46 @@ package org.peerbox.presenter.settings.synchronization;
 
 
 
-import java.awt.MenuItem;
 import java.io.IOException;
 import java.nio.file.Path;
 
+import org.controlsfx.control.action.Action;
+import org.controlsfx.dialog.Dialogs;
+import org.controlsfx.dialog.Dialog;
 import org.peerbox.app.manager.file.IFileManager;
 import org.peerbox.app.manager.user.IUserManager;
 import org.peerbox.interfaces.IFxmlLoaderProvider;
+import org.peerbox.presenter.settings.Properties;
 import org.peerbox.share.IShareFolderHandler;
 import org.peerbox.share.ShareFolderController;
 import org.peerbox.share.ShareFolderHandler;
+import org.peerbox.view.ViewNames;
 import org.peerbox.watchservice.IFileEventManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.Provider;
 
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.cell.CheckBoxTreeCell;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tooltip;
 import javafx.scene.control.CustomMenuItem;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.CheckBoxTreeItem;
+import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import javafx.util.Callback;
 
@@ -47,26 +60,29 @@ public class CustomizedTreeCell extends CheckBoxTreeCell<PathItem> {
 	private ContextMenu menu = new ContextMenu();
 	private Provider<IShareFolderHandler> shareFolderHandlerProvider;
 
+	private Stage stage;
+	
 	public CustomizedTreeCell(IFileEventManager fileEventManager,
 			Provider<IShareFolderHandler> shareFolderHandlerProvider){
 
 		this.shareFolderHandlerProvider = shareFolderHandlerProvider;
-
 		CustomMenuItem deleteItem = new CustomMenuItem(new Label("Delete from network"));
 		Label shareLabel = new Label("Share");
-		shareLabel.setTooltip(new Tooltip("haha"));
 		CustomMenuItem shareItem = new CustomMenuItem(shareLabel);
+		MenuItem propertiesItem = new MenuItem("Properties");
 
 		shareItem.setOnAction(new ShareFolderAction());
 
 		menu.getItems().add(deleteItem);
 		menu.getItems().add(shareItem);
+		menu.getItems().add(propertiesItem);
 
 		menu.setOnShowing(new EventHandler<WindowEvent>() {
 			@Override
 			public void handle(WindowEvent arg0) {
 				if(getItem().isFile()){
 					shareItem.setVisible(false);
+					propertiesItem.setVisible(false);
 				} else if(!getItem().getPath().toFile().exists()){
 					shareItem.setDisable(true);
 					Label label = (Label)shareItem.getContent();
@@ -83,16 +99,77 @@ public class CustomizedTreeCell extends CheckBoxTreeCell<PathItem> {
 				}
 			}
 		});
+		
+		propertiesItem.setOnAction(new EventHandler<ActionEvent>() {
 
-		deleteItem.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent t) {
-				fileEventManager.onLocalFileHardDelete(getItem().getPath());
+			@Override
+			public void handle(ActionEvent arg0) {
+				showProperties(getItem());
+			}
+
+			private void showProperties(PathItem item) {
+				try {
+					FXMLLoader loader = new FXMLLoader();
+					loader.setLocation(getClass().getResource(
+							ViewNames.PROPERTIES_VIEW));
+
+					loader.setController(new Properties(getItem()));
+
+					Parent root = loader.load();
+
+					// load UI on Application thread and show window
+					Runnable showStage = new Runnable() {
+						@Override
+						public void run() {
+							Scene scene = new Scene(root);
+							stage = new Stage();
+							stage.setTitle("Properties of "
+									+ getItem().getPath().getFileName());
+							stage.setScene(scene);
+							stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+								@Override
+								public void handle(WindowEvent event) {
+									stage = null;
+								}
+							});
+
+							stage.show();
+						}
+					};
+
+					if (Platform.isFxApplicationThread()) {
+						showStage.run();
+					} else {
+						Platform.runLater(showStage);
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		});
 
+		deleteItem.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent t) {
+				Action response = Dialogs.create()
+		        .title("You're about to irreversibly hard-delete this file from the network!")
+		        .masthead(null)
+		        .message("Do you really want to hard-delete" + getItem().getPath() + "?" +
+		        		" The file is automatically deleted from all devices and cannot be "
+		        		+ "reconstructed.")
+		        .actions(Dialog.ACTION_OK, Dialog.ACTION_CANCEL)
+		        .showConfirm();
+				if(response == Dialog.ACTION_OK){
+					fileEventManager.onLocalFileHardDelete(getItem().getPath());
+					Dialogs.create()
+					        .title("PeerWasp Information")
+					        .masthead(null)
+					        .message(getItem().getPath() + " has been hard-deleted.")
+					        .showInformation();
+				}
+			}
+		});
         setContextMenu(menu);
 	}
-
 
 	private class ShareFolderAction implements EventHandler<ActionEvent> {
 		@Override
@@ -102,5 +179,5 @@ public class CustomizedTreeCell extends CheckBoxTreeCell<PathItem> {
 			handler.shareFolder(toShare);
 		}
 	}
-
+	
 }
