9872c003bb14f9cd84c1cf37cd593bd738ca1a5a
Andreas Albrecht <albrecht@iodir.com>
2015-03-11 18:27:48 +0100

    + test cases

--- peerbox/src/test/java/org/peerbox/forcesync/RemoteUnknownLocalAdd.java
+++ peerbox/src/test/java/org/peerbox/forcesync/RemoteUnknownLocalAdd.java
@@ -1,5 +1,20 @@
 package org.peerbox.forcesync;
 
-public class RemoteUnknownLocalAdd {
+import java.nio.file.Path;
 
+import org.junit.Test;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+
+public class RemoteUnknownLocalAdd extends ListSyncTest {
+
+	@Test
+	public void testLocalAdd() throws Exception {
+		local.put(filePath, file1);
+
+		listSync.sync(local, localDatabase, remote, remoteDatabase);
+
+		Mockito.verify(fileEventManager).onLocalFileCreated(Matchers.any(Path.class));
+		Mockito.verifyNoMoreInteractions(fileEventManager);
+	}
 }
--- peerbox/src/test/java/org/peerbox/forcesync/RemoteUnknownLocalDelete.java
+++ peerbox/src/test/java/org/peerbox/forcesync/RemoteUnknownLocalDelete.java
@@ -1,5 +1,16 @@
 package org.peerbox.forcesync;
 
-public class RemoteUnknownLocalDelete {
+import org.junit.Test;
+import org.mockito.Mockito;
 
+public class RemoteUnknownLocalDelete extends ListSyncTest {
+	@Test
+	public void testLocalDelete() throws Exception {
+		localDatabase.put(filePath, file1);
+
+		listSync.sync(local, localDatabase, remote, remoteDatabase);
+
+		// no interactions because it does not exist in network and not anymore locally
+		Mockito.verifyNoMoreInteractions(fileEventManager);
+	}
 }
