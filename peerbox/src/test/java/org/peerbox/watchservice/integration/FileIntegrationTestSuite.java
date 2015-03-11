ad63bfee27ca8ae4deaa6524dec3c74e3fbf9961
raute <claudioanliker@hotmail.com>
2015-03-11 18:26:54 +0100

    + Test suite and more tests

--- peerbox/src/test/java/org/peerbox/watchservice/integration/FileIntegrationTestSuite.java
+++ peerbox/src/test/java/org/peerbox/watchservice/integration/FileIntegrationTestSuite.java
@@ -3,7 +3,6 @@ package org.peerbox.watchservice.integration;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-import org.peerbox.watchservice.conflicthandling.ConflictHandler;
 
 @RunWith(Suite.class)
 @SuiteClasses({ 
