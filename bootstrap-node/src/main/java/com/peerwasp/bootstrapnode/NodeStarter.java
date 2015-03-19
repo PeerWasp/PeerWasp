3f2f0ed3cecb353cbbfaa5faed4e4ca844db6b12
Andreas Albrecht <albrecht@iodir.com>
2015-03-19 11:23:16 +0100

    + bootstrap node, removed readme (link to wiki)

--- bootstrap-node/src/main/java/com/peerwasp/bootstrapnode/NodeStarter.java
+++ bootstrap-node/src/main/java/com/peerwasp/bootstrapnode/NodeStarter.java
@@ -1,6 +1,5 @@
 package com.peerwasp.bootstrapnode;
 
-import java.io.IOException;
 import java.net.InetAddress;
 import java.util.UUID;
 
@@ -14,44 +13,54 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Starts a single node.
- * If the node should be created as an initial peer, do not give any command-line arguments.
- * If the node should connect to an already existing peer, give the address as command-line argument.
+ * Starts a single node. If the node should be created as an initial peer, do
+ * not give any command-line arguments. If the node should connect to an already
+ * existing peer, give the address as command-line argument.
  *
  * @author albrecht
  *
  */
-public class Starter {
+public class NodeStarter {
 
-	private static final Logger logger = LoggerFactory.getLogger(Starter.class);
+	private static final Logger logger = LoggerFactory.getLogger(NodeStarter.class);
 
 	public static void main(String[] args) {
 		try {
 
 			INetworkConfiguration netConfig = null;
 
-			if(args.length == 0) {
+			if (args.length == 0) {
+
 				// initial peer
-				logger.info("Start as initial peer");
 				netConfig = NetworkConfiguration.createInitial();
-			} else if(args.length >= 1) {
-				// bootstrap to node
-				logger.info("Connect to existing peer");
+				logger.info("Start as initial peer");
+
+			} else if (args.length >= 1) {
+
+				// connect to node
+				String nodeId = UUID.randomUUID().toString();
 				InetAddress address = InetAddress.getByName(args[0]);
-				netConfig = NetworkConfiguration.create(UUID.randomUUID().toString(),  address);
+				netConfig = NetworkConfiguration.create(nodeId, address);
+				logger.info("Connect to existing peer. Address: {}", address.toString());
+
 			}
 
 			IFileConfiguration fileConfig = FileConfiguration.createDefault();
 			IH2HNode peerNode = H2HNode.createNode(fileConfig);
 			boolean success = peerNode.connect(netConfig);
-			if(!success) {
-				logger.error("Could not connect to other node: {}", args[0]);
+
+			if (success) {
+				logger.info("Setup successful.");
+			} else {
+				logger.error("Could not setup node.");
+				System.exit(-1);
 			}
 
 			// do not exit
+			logger.info("Finished creating node.");
 			Thread.currentThread().join();
 
-		} catch (InterruptedException | IOException e) {
+		} catch (Exception e) {
 			logger.warn("Exception: {}", e.getMessage(), e);
 		}
 
