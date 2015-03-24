dbc3db8ae04a98217f4b0df60f5a7b56e589e08f
Andreas Albrecht <albrecht@iodir.com>
2015-03-24 20:32:02 +0100

    make sure that resource input streams are closed.

--- peerbox/src/main/java/org/peerbox/presenter/settings/synchronization/SynchronizationUtils.java
+++ peerbox/src/main/java/org/peerbox/presenter/settings/synchronization/SynchronizationUtils.java
@@ -1,5 +1,11 @@
 package org.peerbox.presenter.settings.synchronization;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
@@ -11,78 +17,96 @@ import javafx.scene.image.ImageView;
  *
  */
 public class SynchronizationUtils {
-	
+
 	private static final String successTooltip = "This file is synchronized.\n";
 	private static final String errorTooltip = "Synchronization of this file failed.\n";
 	private static final String inProgressTooltip = "Synchronization of this file is currently ongoing.\n";
 	private static final String softDeletedTooltip = "This file is currently not synchronized.\n"
 			+ "Check the box to download it.";
 	private static final String sharedFolderTooltip = "This folder is shared with other users.\n";
-	
+
 	public static String getSuccessTooltip(){
 		return successTooltip;
 	}
-	
+
 	public static String getErrorTooltip(){
 		return errorTooltip;
 	}
-	
+
 	public static String getInProgressToolTip(){
 		return inProgressTooltip;
 	}
-	
+
 	public static String getSoftDeletedTooltip(){
 		return softDeletedTooltip;
 	}
-	
-	public static String getSharedFolderTooltip(){
+
+	public static String getSharedFolderTooltip() {
 		return sharedFolderTooltip;
 	}
-	public static ImageView getFolderSuccessIcon(){
-		return new ImageView(new Image(SynchronizationUtils.class.getResourceAsStream("/images/folder-success.png")));
+
+	public static ImageView getFolderSuccessIcon() {
+		return getImageByName("/images/folder-success.png");
 	}
-	
-	public static ImageView getFolderInProgressIcon(){
-		return new ImageView(new Image(SynchronizationUtils.class.getResourceAsStream("/images/folder-synch.png")));
+
+	public static ImageView getFolderInProgressIcon() {
+		return getImageByName("/images/folder-synch.png");
 	}
-	
-	public static ImageView getFolderErrorIcon(){
-		return new ImageView(new Image(SynchronizationUtils.class.getResourceAsStream("/images/folder-error.png")));
+
+	public static ImageView getFolderErrorIcon() {
+		return getImageByName("/images/folder-error.png");
 	}
-	
-	public static ImageView getFolderStandardIcon(){
-		return new ImageView(new Image(SynchronizationUtils.class.getResourceAsStream("/images/folder-standard.png")));
+
+	public static ImageView getFolderStandardIcon() {
+		return getImageByName("/images/folder-standard.png");
 	}
-	
-	public static ImageView getFileSuccessIcon(){
-		return new ImageView(new Image(SynchronizationUtils.class.getResourceAsStream("/images/file-success.png")));
+
+	public static ImageView getFileSuccessIcon() {
+		return getImageByName("/images/file-success.png");
 	}
-	
-	public static ImageView getFileInProgressIcon(){
-		return new ImageView(new Image(SynchronizationUtils.class.getResourceAsStream("/images/file-synch.png")));
+
+	public static ImageView getFileInProgressIcon() {
+		return getImageByName("/images/file-synch.png");
 	}
-	
-	public static ImageView getFileErrorIcon(){
-		return new ImageView(new Image(SynchronizationUtils.class.getResourceAsStream("/images/file-error.png")));
+
+	public static ImageView getFileErrorIcon() {
+		return getImageByName("/images/file-error.png");
 	}
-	
-	public static ImageView getFileStandardIcon(){
-		return new ImageView(new Image(SynchronizationUtils.class.getResourceAsStream("/images/file-standard.png")));
+
+	public static ImageView getFileStandardIcon() {
+		return getImageByName("/images/file-standard.png");
 	}
-	
+
 	public static ImageView getSharedFolderSuccessIcon(){
-		return new ImageView(new Image(SynchronizationUtils.class.getResourceAsStream("/images/folder-shared.png")));
+		return getImageByName("/images/folder-shared.png");
 	}
-	
-	public static ImageView getSharedFolderInProgressIcon(){
-		return new ImageView(new Image(SynchronizationUtils.class.getResourceAsStream("/images/folder-shared-synch.png")));
+
+	public static ImageView getSharedFolderInProgressIcon() {
+		return getImageByName("/images/folder-shared-synch.png");
 	}
-	
-	public static ImageView getSharedFolderErrorIcon(){
-		return new ImageView(new Image(SynchronizationUtils.class.getResourceAsStream("/images/folder-shared-error.png")));
+
+	public static ImageView getSharedFolderErrorIcon() {
+		return getImageByName("/images/folder-shared-error.png");
 	}
-	
-	public static ImageView getSharedFolderStandardIcon(){
-		return new ImageView(new Image(SynchronizationUtils.class.getResourceAsStream("/images/folder-shared-standard.png")));
+
+	public static ImageView getSharedFolderStandardIcon() {
+		return getImageByName("/images/folder-shared-standard.png");
+	}
+
+	/**
+	 * Loads the given resource and returns a new ImageView instance.
+	 * @param resourceName
+	 * @return image view with associated resource.
+	 */
+	private static ImageView getImageByName(String resourceName) {
+		try (InputStream in = SynchronizationUtils.class.getResourceAsStream(resourceName)) {
+			if (in != null) {
+				return new ImageView(new Image(in));
+			}
+		} catch (IOException e) {
+			Logger logger = LoggerFactory.getLogger(SynchronizationUtils.class);
+			logger.warn("Could not load icon '{}'.", resourceName, e);
+		}
+		return new ImageView();
 	}
 }
