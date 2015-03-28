caccaa627a187d4a9f06fc4f618d502f9557c506
Andreas Albrecht <albrecht@iodir.com>
2015-03-28 16:26:31 +0100

    + added version information to about tab + make about tab scrollable

--- peerbox/src/main/java/org/peerbox/presenter/settings/About.java
+++ peerbox/src/main/java/org/peerbox/presenter/settings/About.java
@@ -3,11 +3,14 @@ package org.peerbox.presenter.settings;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 import org.peerbox.utils.BrowserUtils;
+import org.peerbox.utils.VersionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,9 +18,47 @@ public class About implements Initializable {
 
 	private static final Logger logger = LoggerFactory.getLogger(About.class);
 
+	@FXML
+	private Label lblPeerWaspVersion;
+	@FXML
+	private Label lblH2HVersion;
+	@FXML
+	private Label lblTomP2PVersion;
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		updateVersions();
+	}
 
+	private void updateVersions() {
+		final Runnable setVersions = new Runnable() {
+			@Override
+			public void run() {
+				String peerWaspVersion = VersionUtils.getPeerWaspVersion();
+				if (peerWaspVersion == null || peerWaspVersion.isEmpty()) {
+					peerWaspVersion = "unknown";
+				}
+				lblPeerWaspVersion.setText(peerWaspVersion);
+
+				String h2hVersion = VersionUtils.getH2HVersion();
+				if (h2hVersion == null || h2hVersion.isEmpty()) {
+					h2hVersion = "unknown";
+				}
+				lblH2HVersion.setText(h2hVersion);
+
+				String tomp2pVersion = VersionUtils.getTomP2PVersion();
+				if (tomp2pVersion == null || tomp2pVersion.isEmpty()) {
+					tomp2pVersion = "unknown";
+				}
+				lblTomP2PVersion.setText(tomp2pVersion);
+			}
+		};
+
+		if (Platform.isFxApplicationThread()) {
+			setVersions.run();
+		} else {
+			Platform.runLater(setVersions);
+		}
 	}
 
 	@FXML
